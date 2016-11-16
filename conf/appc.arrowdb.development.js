c6c0a968e6dfeacdad427dd4138adb40179ea4a7
Marin Vasilev <mvasilev@axway.com>
2016-11-16 12:19:42 +0200

    Initial project commit

---
+++ conf/appc.arrowdb.development.js
@@ -0,0 +1,16 @@
+/**
+ * Generated Arrow DB configuration for development
+ */
+module.exports = {
+  "connectors": {
+    "appc.arrowdb": {
+      "enabled": true,
+      "connector": "appc.arrowdb",
+      "environment": "development",
+      "key": "nLePHU1Kaj0KfyysjLLjC7pwCoqbG4HN",
+      "baseurl": "https://api.cloud.appcelerator.com",
+      "username": "appc_app_user_dev",
+      "password": "sx3lghdGCBugSPyGak"
+    }
+  }
+};
---
+++ conf/appc.arrowdb.production.js
@@ -0,0 +1,16 @@
+/**
+ * Generated Arrow DB configuration for production
+ */
+module.exports = {
+  "connectors": {
+    "appc.arrowdb": {
+      "enabled": true,
+      "connector": "appc.arrowdb",
+      "environment": "production",
+      "key": "tyQxGpYoYOLVqcntDuLIq02ms62X8SGi",
+      "baseurl": "https://api.cloud.appcelerator.com",
+      "username": "appc_app_user",
+      "password": "285LAl20eTlAnQDCoH"
+    }
+  }
+};
